3dcd2b897a8e22a9ff31c20780cdff1a5e37a97a
chris.miranda <christophermiranda@pursuit.org>
2023-12-05 17:53:46 -0500

    finished the lab

---
+++ src/Components/NavBar.jsx
@@ -0,0 +1,13 @@
+function NavBar () {
+    return (
+        <div className="nav">
+        <nav>
+            <button>What Is Pursuit?</button>
+            <button>Create an Account</button>
+            <button>Sign In</button>
+        </nav>
+        </div>
+    )
+}
+
+export default NavBar
