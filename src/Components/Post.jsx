dc06652d4362f69fcf955e67f9457eedb2c038cd
Alex_de_code <alexandergonzalez@pursuit.orgclear>
2023-12-05 14:46:20 -0500

    added basic Post.jsx component functionality

---
+++ src/Components/Post.jsx
@@ -0,0 +1,10 @@
+function Post() {
+    return (
+        <div>
+            <img src="" alt="Post Profile Pic" />
+            <p>Jordan Walke</p>
+            <p>React Creator</p>
+            <p>Lorem Ipsum</p>
+        </div>
+    )
+}
