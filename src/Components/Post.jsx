36aab8ffd2466b51a53f181b14651e520390aa41
Isiah Arrufat <isiaharrufat@pursuit.org>
2023-12-06 09:14:37 -0500

    completed lab partically ran out of time

---
+++ src/Components/Post.jsx
@@ -0,0 +1,12 @@
+
+
+const Post = () => {
+    return (
+        <div className="post">
+            <h3>Title</h3>
+            <>Lorem ipsum </>
+        </div>
+    )
+}
+
+export default Post;
---
+++ src/Components/UserProfile.jsx
@@ -0,0 +1,15 @@
+
+const UserProfile = () => {
+    return (
+        <div className="userProfile">
+            <img src="/src/images/profile.png" alt="pic of user" />
+            <div className="userText">
+            <h2> Zaye Willz </h2>
+            <h3>React Creator</h3>
+            <p>Lorem ipsum dolor, sit amet consectetur</p>
+            </div>
+        </div>
+    )
+}
+
+export default UserProfile;
