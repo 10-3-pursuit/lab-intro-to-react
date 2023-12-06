3e679f046e23fca9992bb15979997414adcfb3d4
Jennifer <jenniferpeterson@pursuit.org>
2023-12-05 19:58:11 -0500

    created the Contacts Component

---
+++ src/Components/Contacts.jsx
@@ -0,0 +1,12 @@
+import ContactUserCard from "./ContactUserCard";
+
+const Contacts = () => {
+  return (
+    <aside style={{ border: "2px solid black" }}>
+      <h2>Contacts</h2>
+      <ContactUserCard />
+    </aside>
+  );
+};
+
+export default Contacts;
