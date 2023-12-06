513fae12f5d9e70e420850fff9887e195a1b413b
armandopires <armandopires@pursuit.org>
2023-12-05 22:27:14 -0500

    Update css

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,12 +1,12 @@
 function Navbar() {
     return (
-      <div className="Border">
+      // <div className="Border">
       <nav className="nav-bar">
           <button>What is Pursuit</button>
           <button>Create an Account</button>
           <button>Sign In</button>
       </nav>
-      </div>
+      // </div>
     );
   }
   
