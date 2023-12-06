a95a717c92146fa8ce7c96d993ef2ec5677643d0
Jennifer <jenniferpeterson@pursuit.org>
2023-12-05 20:00:03 -0500

    created Post Component

---
+++ src/Components/Post.jsx
@@ -0,0 +1,13 @@
+const Post = () => {
+  return (
+    <>
+      <div style={{ border: "2px solid black", textAlign: "center" }}>
+        <h2>Title</h2>
+        <p>Lorem Ipsum</p>
+      </div>
+      <br />
+    </>
+  );
+};
+
+export default Post;
