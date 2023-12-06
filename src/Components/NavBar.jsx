e6ee35c38d26a8141d208e0c86a9f0d652cea09c
Carlitos <carlitosdutan@pursuit.org>
2023-12-06 18:22:41 -0500

    Lab Done

---
+++ src/Components/NavBar.jsx
@@ -0,0 +1,12 @@
+function NavBar() {
+    return (
+        <div className="navbar">
+            <a href="#">What is Pursuit?</a>
+            <a href="#">Create an Account</a>
+            <a href="#">Sign In</a>
+        </div>
+    )
+
+}
+
+export default NavBar;
---
+++ src/Components/Post.jsx
@@ -0,0 +1,10 @@
+function Post() {
+    return(
+        <div className="post">
+            <h2>Title</h2>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestiae delectus blanditiis facere nostrum animi porro tenetur esse voluptates dolore, ullam quaerat aut praesentium quos inventore? Illum tenetur excepturi voluptate nulla?</p>
+        </div>
+    )
+}
+
+export default Post;
