3a5a275efdd5b71e900cfc7b6e57dd51d69b057f
Jose Barrios <josebarrios@pursuit.org>
2023-12-05 22:13:21 -0500

    made changes to user profile card and css

--- src/Components/UserProfile.jsx
+++ src/Components/UserProfile.jsx
@@ -1,10 +1,28 @@
-import ContactUserCard from "./ContactUserCard";
+//OTHER OPTION BELOW
+// import ContactUserCard from "./ContactUserCard";
+
+// function UserProfile() {
+//     return(
+//         <div>
+//             <ContactUserCard/>
+//         </div>
+//     )
+// }
+
+// export default UserProfile;
+
+import profilePic from '../images/profile.png';
 
 function UserProfile() {
     return(
+        <div className='user-profile-card'>
+        <img src={profilePic} alt="" />
         <div>
-            <ContactUserCard/>
+        <h3>Someone</h3>
+        <h4>Their Job</h4>
+        <p>Something About Them</p>
         </div>
+      </div>
     )
 }
 
