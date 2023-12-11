c58add4741bbb9b63a34c0890d994b62f03fedaf
Iris Beckham <irisdancer3@gmail.com>
2023-12-11 15:54:41 -0800

    posts component

---
+++ src/Components/Post.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Post = () => {
+    return (
+        <li>
+            <h2>Title</h2>
+            <p>Lorem ipsem</p>
+        </li>
+    )
+}
+
+export default Post
