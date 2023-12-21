847c7818e0dc8cb9607b948b95414279559f3d4b
Lenny Lopez <lennylopez@pursuit.org>
2023-12-21 16:23:46 -0500

    LabIntroReact

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,26 @@
-import React from "react";
-import "./index.css";
+import React from 'react';
+import NavBar from './components/NavBar';
+import UserProfile from './components/UserProfile';
+import Posts from './components/Posts';
+import Contacts from './components/Contacts';
+import './index.css';
 
-function App() {
-  return <p>Hello, world!</p>;
-}
+const App = () => {
+  return (
+    <div className="app-container">
+      <NavBar />
+      <div className="content-container">
+        <div className="left-content">
+          <UserProfile />
+          <Posts />
+        </div>
+        <div className="right-content">
+          <Contacts />
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default App;
+
---
+++ src/Components/UserProfile.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const ProfileHeader = () => {
+  return (
+    <div className="profile-header">
+      <img src="blank photo box.webp" alt="Profile" className="profile-picture" />
+      <h1>John Doe</h1>
+      <p>Web Developer</p>
+      
+    </div>
+  );
+};
+
+export default ProfileHeader;
